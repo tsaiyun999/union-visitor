a242e943596eed2de075b934659e0575b1e0667e
tsaiyun999 <ansto0909@gmail.com>
2025-09-01 15:36:53 +0800

    new commit

--- app/building/[name]/page.js
+++ app/building/[name]/page.js
@@ -33,12 +33,12 @@ export default function Building({ params }) {
         </div>
       </div>
 
-      <a
+      <Link
         href="/"
         className="inline-block mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
         返回首頁
-      </a>
+      </Link>
     </div>
   );
 }
